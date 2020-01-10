7132f3391e3872d7deaa6c59e9d0ef23e36c904f
hiraith <hiraith@naver.com>
2020-01-10 22:32:18 +0900

    working error fix

---
+++ src/main/java/com/upffice/model/WorkingDtoPK.java
@@ -0,0 +1,8 @@
+package com.upffice.model;
+
+import java.io.Serializable;
+
+public class WorkingDtoPK implements Serializable {
+    private int empId;
+    private String workingDate;
+}
--- src/main/java/com/upffice/repo/WorkingRepository.java
+++ src/main/java/com/upffice/repo/WorkingRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
+
 public interface WorkingRepository extends CrudRepository<WorkingDto, Integer> {
 }

b741577104918f4d96bbb281e85ab1d6021febec
hiraith <hiraith@naver.com>
2020-01-10 22:32:18 +0900

    working error fix

--- src/main/java/com/upffice/model/WorkingDto.java
+++ src/main/java/com/upffice/model/WorkingDto.java
@@ -1,4 +1,26 @@
 package com.upffice.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "working")
+@IdClass(WorkingDtoPK.class)
 public class WorkingDto {
+    @Id
+    @Column(name = "emp_id")
+    private int empId;
+
+    @Id
+    @Column(name = "working_date")
+    private String workingDate;
+
+    @Column(name = "working_in")
+    private int workingIn;
+
+    @Column(name = "working_out")
+    private boolean workingOut;
+
+    @Column(name = "working_time")
+    private boolean workingTime;
+
 }

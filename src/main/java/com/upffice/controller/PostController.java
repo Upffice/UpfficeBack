5ad0f21ccbd018155484494a36f6b73c58fedac5
Jung-jun-hee <wjdwnsgml463@naver.com>
2020-01-16 16:31:00 +0900

    Controller 주석처리

--- src/main/java/com/upffice/controller/PostController.java
+++ src/main/java/com/upffice/controller/PostController.java
@@ -18,14 +18,16 @@ public class PostController {
     @Autowired
     PostRepository repository;
 
+    // 게시글 저장 하는 메소드
         @PostMapping("/post")
         public PostDto postBoard(@RequestBody PostDto postDto){
             System.out.println(postDto + "post 찍어보기");
             PostDto _post=repository.save(new PostDto(postDto.getBoard_name(), postDto.getHeader(), postDto.getPost_writer(), postDto.getPost_subject(), postDto.getPost_content()));
             System.out.println(_post+"_post");
-            return _post; // 게시글 저장 하는 메소드
+            return _post;
     }
 
+    //저장된 게시물을 보내주는 메소드
     @GetMapping("/posts")
     public List<PostDto> getAllPosts(){
 
@@ -33,7 +35,7 @@ public class PostController {
             repository.findAll().forEach(posts::add);
 
             return posts;
-    } // 저장된 게시물 뿌려주는애
+    }
     @DeleteMapping("/delete/{post_id}")
     public ResponseEntity<String> deletePost(@PathVariable("post_id")int post_id){
             System.out.println("Delete Customer with ID =" + post_id+"...");
@@ -41,6 +43,7 @@ public class PostController {
 
             return new ResponseEntity<>("Customer has been deleted!", HttpStatus.OK);
     }
+    //이 기능을 수행하면 조회수가 +1 되어 DB에 저장되는 메서드
     @PutMapping("/view/{post_id}")
     public ResponseEntity<PostDto> updateView(@PathVariable("post_id") int post_id){
         System.out.println("get post_view");
@@ -56,15 +59,12 @@ public class PostController {
         }
 
     }
+    //post_id 값의 모든 데이터를 불러와 수정하는 메서드
     @PutMapping("/update/{post_id}")
     public void updatePost(@PathVariable("post_id")int post_id, @RequestBody PostDto updateData) {
         System.out.println("post_id, 수정데이터 "+ post_id +"  "+ updateData);
         repository.updatePost(updateData.getPost_subject(), updateData.getPost_content(), null, post_id);
     }
-//    @PutMapping("/update/{post_id}")
-//    public void updatePost(@PathVariable("post_id")String post_id) {
-//        System.out.println("post_id, 수정데이터 "+ post_id);
-//    }
 
 
 

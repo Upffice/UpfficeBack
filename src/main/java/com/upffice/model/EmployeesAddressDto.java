f31f1d65a0ff6b3d31f6e1350db6c1dea29ca91f
jeonelfin <jeonregina11@gmail.com>
2020-02-06 13:48:02 +0900

    /

--- src/main/java/com/upffice/model/EmployeesAddressDto.java
+++ src/main/java/com/upffice/model/EmployeesAddressDto.java
@@ -8,7 +8,6 @@ import java.sql.Date;
 @Table(name = "employees")
 public class EmployeesAddressDto {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private int emp_id;
 
     @Column(name = "emp_pw")

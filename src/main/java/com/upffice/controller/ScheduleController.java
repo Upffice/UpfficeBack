bb2ad559e7f603e3f6b38208dbde8975610270ab
Jiyeong-Kim <zeelungx7@naver.com>
2020-02-07 17:01:37 +0900

    캘린더 전체일정 가져오기 외 필요없는 메소드 제거

--- src/main/java/com/upffice/controller/ScheduleController.java
+++ src/main/java/com/upffice/controller/ScheduleController.java
@@ -35,37 +35,18 @@ public class ScheduleController {
     /* UpfficeFront 의 Calendar.vue의 getAllSchedule() 메소드와 매핑됨 : 모든 schedule 리스트를 emp_id 로 가져오기 */
     @PostMapping("/all/{emp_id}")
     public List<ScheduleDto> getAllSchedules(@PathVariable("emp_id") int emp_id, @RequestBody String date) throws ParseException {
-        System.out.println("getAllSchedules() 들어오나 보자 : emp_id " +"- "+emp_id + "/ date : " + date);
-        SimpleDateFormat dt = new SimpleDateFormat("yyyyy-mm-dd");
+        int i=0;
+        System.out.println("getAllSchedules() 들어오나 보자 : emp_id " + "-" + emp_id + "/ date : " + date);
+        SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd");
         Date date1 = dt.parse(date);
         List<ScheduleDto> _scheduleDto = repository.getScheduleByEmpId(emp_id, date1);
+        System.out.println(i++ +"번 째 date " + date1);
 
         for(ScheduleDto d : _scheduleDto) {
             System.out.println("all + " + d);
         }
 
         return _scheduleDto;
-    }
-
-    /* 날짜, 카테고리에 해당되는 일정만....? */
-    @PostMapping("/list/{emp_id}")
-    public List<ScheduleDto> getSchedule(@PathVariable("emp_id") int emp_id, @RequestBody int[] schedules, @RequestBody String date) {
-        System.out.println(schedules + "schedules");
-        List<ScheduleDto> schedule = new ArrayList<>();
-
-        // schedule 배열 안에 포함된 sche_id 에 해당 되고
-        for(int i=0; i<schedules.length; i++) {
-            // start_date 와 end_date 사이에 date 가 포함된 일정 불러오기
-            List<ScheduleDto> schedule2 = new ArrayList<>();
-            schedule2 = repository.getScheduleByScheId(emp_id, schedules[i], date);
-            schedule.addAll(schedule2); // list 끼리 합치기
-        }
-
-        for(ScheduleDto d : schedule) {
-            System.out.println("리스트 + " + d);
-        }
-
-        return schedule;
     }
 
 }
--- src/main/java/com/upffice/repo/ScheduleRepository.java
+++ src/main/java/com/upffice/repo/ScheduleRepository.java
@@ -12,15 +12,11 @@ public interface ScheduleRepository extends CrudRepository<ScheduleDto, Integer>
      * Schedule 테이블에 해당하는 Repository
      */
 
-//    @Query("SELECT DISTINCT s FROM ScheduleDto s WHERE s.emp_id=?1 AND s.sche_start_date <= ?2 and s.sche_end_date >= ?2")
-//    List<ScheduleDto> getScheduleByEmpId(int emp_id, Date date);
-
-    @Query("SELECT DISTINCT s FROM ScheduleDto s WHERE s.emp_id=?1 AND ?2 BETWEEN s.sche_start_date and s.sche_end_date")
+    // between 써서 날짜 사이에 ?1 가 끼어있는 일정들 출력 - 전부 출력
+    @Query("SELECT DISTINCT s FROM ScheduleDto s WHERE s.emp_id=?1 AND ?2 BETWEEN s.sche_start_date AND s.sche_end_date")
     List<ScheduleDto> getScheduleByEmpId(int emp_id, Date date);
 
-    // between 써서 날짜 사이에 ?1 가 끼어있는 일정들 출력 - 전부 출력, calendarId 로 구분해 출력하는 메소드 총 2개
-    @Query(value="select s from ScheduleDto s where s.emp_id=?1 and s.sche_id =?2 " +
-            "and s.sche_start_date <= STR_TO_DATE(?3, '%Y-%m-%d') and s.sche_end_date >= STR_TO_DATE(?3, '%Y-%m-%d')"
-            , nativeQuery=true)
+    // between 써서 날짜 사이에 ?1 가 끼어있는 일정들 - calendarId 로 캘린더 목록 구분해 출력
+    @Query("SELECT DISTINCT s FROM ScheduleDto s WHERE s.emp_id=?1 AND s.sche_id=?2 AND ?3 BETWEEN s.sche_start_date and s.sche_end_date")
     List<ScheduleDto> getScheduleByScheId(int emp_id, int sche_id, String date);
 }

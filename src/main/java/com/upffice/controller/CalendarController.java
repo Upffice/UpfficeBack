9f8ebc20191e770eeb4d743aa08a3d8779de1e11
Jiyeong-Kim <zeelungx7@naver.com>
2020-02-11 15:14:20 +0900

    Calendar 수정 기능 추가

--- src/main/java/com/upffice/controller/CalendarController.java
+++ src/main/java/com/upffice/controller/CalendarController.java
@@ -5,6 +5,7 @@ import com.upffice.repo.CalendarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,9 +22,27 @@ public class CalendarController {
 
     /* UpfficeFront 의 ScheduleSubMenu.vue의 getCalendarList() 메소드와 매핑됨 : 모든 calendar 리스트를 emp_id 로 가져오기 */
     @GetMapping("/list/{emp_id}")
-    public List<CalendarDto> getCalendar(@PathVariable("emp_id") int emp_id) {
+    public List<CalendarDto> getAllCalendar(@PathVariable("emp_id") int emp_id) {
         List<CalendarDto> _calendarDto = repository.getCalendarByEmpId(emp_id);
 
+        return _calendarDto;
+    }
+
+    /* UpfficeFront 의 ScheduleModifyModal.vue의 getCheckedCalendars() 메소드와 매핑됨 : 모든 calendar 리스트를 emp_id 로 가져오기 */
+    @GetMapping("/checkedList/{emp_id}")
+    public List<CalendarDto> getCheckedCalendar(@PathVariable("emp_id") int emp_id, @RequestParam("calendar_id") String calendars) {
+        List<CalendarDto> _calendarDto = new ArrayList();
+        CalendarDto calendarDto;
+
+        String[] calendars_id = calendars.split(",");   // 콤마 기준으로 String 나누기
+        int[] cal_ids = Arrays.stream(calendars_id).mapToInt(Integer::parseInt).toArray();  //  String 배열을 int 배열로 변환
+
+        for(int i=0; i<cal_ids.length; i++) {
+            calendarDto = repository.getCalendarByCalId(emp_id, cal_ids[i]);
+            _calendarDto.add(calendarDto);
+        }
+
+        System.out.println(_calendarDto);
         return _calendarDto;
     }
 
@@ -51,19 +70,12 @@ public class CalendarController {
         return del_count;
     }
 
-    /* UpfficeFront 의 ScheduleSubMenu.vue 에서 캘린더 수정 버튼 눌렀을 때 매핑 됨 : 캘린더 이름 수정 */
+    /* UpfficeFront 의 ScheduleModifyModal.vue 에서 캘린더 수정 버튼 눌렀을 때 매핑 됨 : 캘린더 이름 수정 */
     @PutMapping("/update/{emp_id}")
-    public int updateCalendarName(@PathVariable("emp_id") int emp_id, @RequestBody String[] calendars) {
+    public int updateCalendar(@PathVariable("emp_id") int emp_id, @RequestBody CalendarDto calendar) {
         int chk = 0;
 
-        for(int i=0; i<calendars.length; i++) {
-            String[] calendarInfo = new String[2];
-            System.out.println(calendars[i]);
-            calendarInfo = calendars[i].split(",");
-
-            // 수정 된 행의 갯수를 받아온다. chk이 1이면 1개의 row가 수정 되었다는 뜻.
-            chk += repository.updateCalendar( calendarInfo[1], emp_id, Integer.parseInt(calendarInfo[0]) );
-        }
+        chk = repository.updateCalendar(calendar.getCalendar_name(), calendar.getCalendar_color(), emp_id, calendar.getCalendar_id());
 
         return chk;
     }
--- src/main/java/com/upffice/repo/CalendarRepository.java
+++ src/main/java/com/upffice/repo/CalendarRepository.java
@@ -18,16 +18,19 @@ public interface CalendarRepository extends CrudRepository<CalendarDto, Integer>
     @Query("SELECT c FROM CalendarDto c WHERE c.emp_id=?1")
     List<CalendarDto> getCalendarByEmpId(int empId);
 
+    /* emp_id 와 calendar_id 로 캘린더 리스트 가져오기 */
+    @Query("SELECT c FROM CalendarDto c WHERE c.emp_id=?1 AND c.calendar_id=?2")
+    CalendarDto getCalendarByCalId(int emp_id, int calendar_id);
+
     /* emp_id 와 calendar_id 로 캘린더 목록 삭제 */
     @Modifying
     @Transactional
     @Query("DELETE FROM CalendarDto WHERE emp_id=?1 AND calendar_id=?2")
     int deleteCalendar(int emp_id, int calendar_id);
 
-    /* emp_id와 calendar_id 로 캘린더 이름 수정*/
+    /* emp_id와 calendar_id 로 캘린더 이름, 색 수정*/
     @Modifying
     @Transactional
-    @Query("UPDATE CalendarDto SET calendar_name=?1 WHERE emp_id=?2 AND calendar_id=?3")
-    int updateCalendar(String calendar_name, int emp_id, int calendar_id);
-
+    @Query("UPDATE CalendarDto SET calendar_name=?1, calendar_color=?2 WHERE emp_id=?3 AND calendar_id=?4")
+    int updateCalendar(String calendar_name, String calendar_color, int emp_id, int calendar_id);
 }

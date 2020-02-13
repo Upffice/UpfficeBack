8b3d71ee647dd3ace77b54fda59d5a6dd2e85f95
Jung-jun-hee <wjdwnsgml463@naver.com>
2020-02-13 10:12:31 +0900

    JH_Back 합침

--- src/main/java/com/upffice/controller/PostController.java
+++ src/main/java/com/upffice/controller/PostController.java
@@ -3,14 +3,11 @@ package com.upffice.controller;
 import com.upffice.model.PostDto;
 import com.upffice.repo.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
